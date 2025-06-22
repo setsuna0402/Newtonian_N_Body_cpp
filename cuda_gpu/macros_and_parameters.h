442149167a4bb3b1265e595f7159500f3f74ba51
setsuna0402 <mayuyuqueen@gmail.com>
2025-06-22 02:15:49 +0100

    update macros for GPU version

--- cuda_gpu/macros_and_parameters.h
+++ cuda_gpu/macros_and_parameters.h
@@ -2,11 +2,15 @@
 #define __macros_and_parameters_h_
 
 // single/double precision
-#define FLOAT8  // on: double precision (If you read IC, use double precision)
+// #define FLOAT8     // on: double precision (If you read IC, use double precision)
 // Use openMP for parallel computing
-#define OPEN_MP // on: multiple threads (OpenMP); off: single thread 
-#define SIMD    // on: use SIMD (AVX2, AVX512, etc.)
-#define DEBUG   // on: debug mode and print debug information
+// #define OPEN_MP    // on: multiple threads (OpenMP); off: single thread 
+// #define SIMD       // on: use SIMD (AVX2, AVX512, etc.)
+#define DEBUG      // on: debug mode and print debug information
+// WARNING: You can only use one of the following GPU options at a time
+#define GPU_SLOW   // on: test GPU code (simplest and slowest method)
+// #define GPU_FAST   // on: use **per-thread** registers memory in GPU code
+// #define GPU_SHARED // on: use **per-thread** registers and shared memory in GPU code
 
 #ifdef OPEN_MP
     #define OPENMP_NUM_THREAD 8
@@ -60,6 +64,10 @@ typedef float real;
 #define N_STEP_MAX      1000000          // Maximum number of evolution steps
 #define N_DUMP_STEP     1000            // Number of dump steps
 #define N_DUMP_MAX      (N_STEP_MAX / N_DUMP_STEP) // Maximum number of dump files
+// WARNING: It is better to set BLOCK_SIZE as a multiple of 32 (Warp size in GPU)
+#define GPU_BLOCK_SIZE  256   // max = 1024!
+// GRID_SIZE = (N_PARTICLE + BLOCK_SIZE - 1) / BLOCK_SIZE
+#define GPU_GRID_SIZE   4  // max = 65535!
 
 // Gobal constants
 extern const real Gc_GravConst_mks;  // Gravitational constant in mks unit
@@ -76,4 +84,19 @@ extern real G_Mass_CodeUnits      ;  // Convert mass from code unit to kg
 extern real G_Velocity_CodeUnits  ;  // Convert velocity from code unit to meter/second
 extern real G_Energy_CodeUnits    ;  // Convert energy from code unit to Joule
 
+// Gobal GPU constants
+extern __constant__ real d_Gc_GravConst_mks;  // Gravitational constant in mks unit
+extern __constant__ real d_Gc_GravConst_cgs;  // Gravitational constant in cgs unit
+extern __constant__ real d_Gc_sqrt_pi      ;  // sqrt(pi)
+extern __constant__ real d_Gc_Mpc_to_km    ;  // 1Mpc = 3.08567758128E+19 km
+extern __constant__ real d_Gc_Mpc_to_m     ;  // 1Mpc = 3.08567758128E+22 m
+extern __constant__ real d_Gc_Mpc_to_cm    ;  // 1Mpc = 3.08567758128E+24 cm
+extern __constant__ real d_Gc_Boxsize      ;  // in code unit
+extern __constant__ real d_Gc_End_time     ;  // Endtime of simulation (code unit)
+// Gobal GPU variables
+extern __device__ real d_G_Length_CodeUnits    ;  // Convert distance from code unit to meter
+extern __device__ real d_G_Time_CodeUnits      ;  // Convert time from code unit to second
+extern __device__ real d_G_Mass_CodeUnits      ;  // Convert mass from code unit to kg
+extern __device__ real d_G_Velocity_CodeUnits  ;  // Convert velocity from code unit to meter/second
+extern __device__ real d_G_Energy_CodeUnits    ;  // Convert energy from code unit to Joule
 #endif
