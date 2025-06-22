e0c0c7755d20cf5959ab85ccffef6e395b882aa0
setsuna0402 <mayuyuqueen@gmail.com>
2025-06-22 22:00:09 +0100

    update var name and description

--- cuda_gpu/UpdatePosition_GPU.cu
+++ cuda_gpu/UpdatePosition_GPU.cu
@@ -5,6 +5,7 @@
 /  written by: KH 
 /  date:       2025/6/22
 /  Input:
+/  dt  (const real) : Timestep for the update
 /  Pos  (Dynamical array) : Position of each particle (Size: N x 3)
 /  Vel  (Dynamical array) : Velocity of each particle (Size: N x 3)
 /  Size (const uint) : Number of particles
@@ -16,10 +17,10 @@
 __global__ void UpdatePosition_GPU(real dt, real (*Pos)[3], real (*Vel)[3],
                                    const uint Size){
     // get the target particle index of each thread
-    const int p = blockDim.x*blockIdx.x + threadIdx.x;
-    if (p < Size) {
-        Pos[p][0] += Vel[p][0] * dt;
-        Pos[p][1] += Vel[p][1] * dt;
-        Pos[p][2] += Vel[p][2] * dt;
+    const int i = blockDim.x*blockIdx.x + threadIdx.x;
+    if (i < Size) {
+        Pos[i][0] += Vel[i][0] * dt;
+        Pos[i][1] += Vel[i][1] * dt;
+        Pos[i][2] += Vel[i][2] * dt;
     }
 } // FUNCTION : UpdatePosition_GPU
--- cuda_gpu/UpdateVelocity_GPU.cu
+++ cuda_gpu/UpdateVelocity_GPU.cu
@@ -5,6 +5,7 @@
 /  written by: KH 
 /  date:       2025/6/22
 /  Input:
+/  dt  (const real) : Timestep for the update
 /  Vel  (Dynamical array) : Velocity of each particle (Size: N x 3)
 /  Acc  (Dynamical array) : Acceleration of each particle (Size: N x 3)
 /  Size (const uint) : Number of particles
@@ -16,11 +17,11 @@
 __global__ void UpdateVelocity_GPU(real dt, real (*Vel)[3], real (*Acc)[3],
                                    const uint Size){
     // get the target particle index of each thread
-    const int p = blockDim.x*blockIdx.x + threadIdx.x;
-    if (p < Size) {
-        Vel[p][0] += Acc[p][0] * dt;
-        Vel[p][1] += Acc[p][1] * dt;
-        Vel[p][2] += Acc[p][2] * dt;
+    const int i = blockDim.x*blockIdx.x + threadIdx.x;
+    if (i < Size) {
+        Vel[i][0] += Acc[i][0] * dt;
+        Vel[i][1] += Acc[i][1] * dt;
+        Vel[i][2] += Acc[i][2] * dt;
     }
 
 } // FUNCTION : UpdateVelocity_GPU
